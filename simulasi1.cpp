69ef1fd50e5bf96d8e1481669289328fd17e9f21
Haris Nasrullah <harisnasrullah212@gmail.com>
2020-05-27 19:57:44 +0700

    polosan

---
+++ simulasi1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void cekBilangan(){
+	bool bil;
+
+}
+
+void cekGanjilGenap(){
+	if (cekBilangan)
+	{
+		
+	}
+}
+
+void cekJumlahKata(){
+
+}
+
+void cekKalimat(){
+	bool kalimat;
+
+}
+
+int main()
+{
+	
+	return 0;
+}
