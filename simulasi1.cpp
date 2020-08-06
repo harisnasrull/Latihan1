29386da0d2a8095cdfd75ee7c2b9af1ed1021f00
arifprbdi <arifprbdi16@gmail.com>
2020-08-06 09:32:11 +0700

    ganti line 7 ririsnikah

--- simulasi1.cpp
+++ simulasi1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int cekBilangan(){
 	bool bil;
-	char str[] ="arf1612200prbdi";
+	char str[] ="RIRISNIKAH202022";
 	
 	cout<<"Yang merupakan Bilangan : "<<endl;
 	for (int i=0; i<strlen(str); i++)
