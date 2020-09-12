8d8d953e724b4c43f4212b43e6e0dad4dca13cd9
Haris Nasrulloh <47680445+harisnasrull@users.noreply.github.com>
2020-09-12 22:32:10 +0700

    Revert "test"

---
+++ halo.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Halo lurr"<<endl;
+	cout<<"Ini Kalimat ke 2"<<endl;
+	cout<<"Ini Kalimat ke 3"<<endl;
+	return 0;
+	cout<<"Ini Kalimat ke 4"<<endl;
+	cout<<"Ini Kalimat ke 5"<<endl;
+}
---
+++ testAndroid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout<<"test";
+	cout<<"Test Sugeb";
+
+
+
+
+
+
+
+    
+
+    return 0;
+}
